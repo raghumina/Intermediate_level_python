60df06141ed20b460a5f40d9dea23cf46a94b93b
raghu <raghumina11@gmail.com>
2020-09-19 21:34:33 +0530

    Update Scope_global_variable_and_keyword.py

--- Scope_global_variable_and_keyword.py
+++ Scope_global_variable_and_keyword.py
@@ -11,13 +11,37 @@
 # whereas a local variable will only present inside a fiunction or block of code and works only in it its presence is local
 
 # for example
+'''
+num1 = 10  # Global
 
-num1 = 10 # Global
+
 def num(n):
- #   num1 = 5  # taking its value from local variable
+    #   num1 = 5  # taking its value from local variable
     print(num1)  # after commenting local variable it takes its value from the global variable
     print("hello")
 
+
 num("hello world!")
-print(num1)   # takes its value from the global variable
+print(num1)  # takes its value from the global variable
+'''
+# inside a function a varible first search value form local variable. if local variable is not present in a function then
+# it searches from global variable if global variable dont have that related variable value then it throws error
+# we can use a global variable inside a function or outside of it but we cant use a local variable outside its block or function
+
+# GLOBAL KEYWORD IF WE WANT TO CHANGE THE VALUE OF A LOCAL VARIABLE IN A SPECIFIC FUNCTION OR CHUNK OF CODE THEN WE
+# USES GLOBAL KEYWORD WITHOUT IT WE CANT CHANGE A GLOBAL VARIABLE FOR A SPECIFIC FUNCTION
+
+# FOR EXAMPLE
+n = 5
+
+
+def change():
+    global n   # global keyword gives permission to change its value in specific function
+    n = n + 23  #
+
+    print(n, "hurray we changed the value with help of global keyword")
+
+
+print(change())
+
 
