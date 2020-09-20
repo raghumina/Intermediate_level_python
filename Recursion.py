08a1c56ef44c886c7d03e8fa90e23b24a6e814da
raghu <raghumina11@gmail.com>
2020-09-20 20:13:40 +0530

    Update Recursion.py

--- Recursion.py
+++ Recursion.py
@@ -1,2 +1,15 @@
 # Recursion
 # Recursion is a method of programming or coding a problem, in which a function calls itself one or more times in its body.
+
+
+# lets start with an example
+#
+# factorial of a nmumber
+def factorial(n):
+    fact = 1
+    for i in range(n):
+        fact = fact * (i + 1)
+        return fact
+
+num = int(input("Enter a number: "))
+print(factorial(num))
