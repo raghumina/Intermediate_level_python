4e6d63f5ce8df132d91d41e548feb8c2f2ca5d05
raghu <raghumina11@gmail.com>
2020-09-21 18:24:01 +0530

    Create Lambda.py

---
+++ Lambda.py
@@ -0,0 +1,23 @@
+# Lambda
+# Lambda functions
+# lambda functions are generally for convenience
+# also known as anonymous func
+# they convert program to one liners
+
+# for example
+# a normal program
+
+def minus(a,b):
+    return a - b
+
+a = 10
+b = 5
+print(minus(a, b))
+
+# know lets use a lambda function
+
+subs = lambda x,y: x - y
+x = 5
+y = 5
+print(subs(x,y))  # well this is the lambda function or one liner function or anonymous function
+
