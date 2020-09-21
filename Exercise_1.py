16143030dce184f3ebeec3bdf63562a4c031a37c
raghu <raghumina11@gmail.com>
2020-09-21 20:57:54 +0530

    Create Exercise_1.py

---
+++ Exercise_1.py
@@ -0,0 +1,32 @@
+# Problem
+# # game: Stone Paper Secissor
+import random
+'''
+list = ["Stone","Paper","Scissor"]
+machine_choice = random.choice(list)
+#print(choice)
+user1 = str(input("Please enter Stone or Paper or Scissor: " ))
+#user1 = user.upper()
+if user1 == "Stone" and machine_choice == "Stone":
+    print("Game tied")
+elif user1 == "Stone" and machine_choice == "Paper":
+    print("machine won")
+elif user1 == "Stone" and machine_choice == "Scissor":
+    print("User won")
+elif user1 == "Paper" and machine_choice == "Paper":
+    print("Game tied")
+elif user1 == "Paper" and machine_choice == "Stone":
+    print("User won")
+elif user1 == "Paper" and machine_choice == "Scissor":
+    print("Machine won")
+elif user1 == "Scissor" and machine_choice == "Stone":
+    print("Machine won")
+elif user1 == "Scissor" and machine_choice == "Paper":
+    print("User won")
+elif user1 == "Scissor" and machine_choice == "Scissor":
+    print("Game tied")
+else:
+    print("Please enter value correctly")
+ '''
+# lets do it with a function:
+# or in a better way
