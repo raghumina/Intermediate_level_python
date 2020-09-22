3c7e321b883b8148fe65d963b754548df264644d
raghu <raghumina11@gmail.com>
2020-09-22 19:14:49 +0530

    Update Args_kwargs.py

--- Args_kwargs.py
+++ Args_kwargs.py
@@ -5,21 +5,34 @@
 # lets start:
 #
 
-#def names(a,b,c,d):
- #   print(a,b,c,d)
+# def names(a,b,c,d):
+#   print(a,b,c,d)
 
-#names("tom","Jerry","kimi","tofa")
+# names("tom","Jerry","kimi","tofa")
 
 # but in this simple functions we cant add or remove the numbewr of arguments in the function
 # So this is not suitable for big programs where input can be more than expectations so we use *arg for this
 
 def arg(*args):
     print(args)
-num = [1,2,3,4,5,6,7]
+
+
+num = [1, 2, 3, 4, 5, 6, 7]
 arg(*num)
 num.append(22)
-arg(*num)           # so without any error it adds or update the list
+arg(*num)  # so without any error it adds or update the list
 num.pop()
 arg(*num)
 
 
+# we can also try it with conditional statements
+def name(*args):  # we can change it name  like *argsname
+    for name1 in names:    # * args are optional
+        print(name1)
+
+
+names = ["tom", "jerry", "larry"]
+name(*names)
+names.append("kolli")
+name(*names)
+# we can add multiple arguments in a function with *args
