8047a06ecf234fa9961d0134e46fbe1e61d36b0a
raghu <raghumina11@gmail.com>
2020-09-22 17:33:52 +0530

    Update Exercise_1.py

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,6 +1,7 @@
 # Problem
 # # game: Stone Paper Secissor
 import random
+
 '''
 list = ["Stone","Paper","Scissor"]
 machine_choice = random.choice(list)
@@ -30,3 +31,56 @@ else:
  '''
 # lets do it with a function:
 # or in a better way
+
+print("This is a Stone Paper Secissor game")
+print("In this game you will play with computer")
+print("Rules are simple ")
+
+print("Lets Play")
+import random
+
+chance = 0
+
+
+def chances():
+    while chance >= 10:
+        print("Uou loose ")
+
+    #  print("You LOOSE")
+
+
+def user_input(user1):
+    if user1 == "Stone" and machine_choice == "Stone":
+        return "Game tied"
+
+    elif user1 == "Stone" and machine_choice == "Paper":
+        return "machine won"
+
+    elif user1 == "Stone" and machine_choice == "Scissor":
+        return "User won"
+
+    elif user1 == "Paper" and machine_choice == "Paper":
+        return "Game tied"
+
+    elif user1 == "Paper" and machine_choice == "Stone":
+        return "User won"
+
+    elif user1 == "Paper" and machine_choice == "Scissor":
+        return "Machine won"
+
+    elif user1 == "Scissor" and machine_choice == "Stone":
+        return "Machine won"
+
+    elif user1 == "Scissor" and machine_choice == "Paper":
+        return "User won"
+    
+    elif user1 == "Scissor" and machine_choice == "Scissor":
+        return "Game tied"
+    else:
+        print("Please enter value correctly")
+
+
+user_choice = ["Stone", "Paper", "Scissor"]
+machine_choice = random.choice(user_choice)
+user1 = input("Enter your choice here: Stone, Paper, Scissor: ")
+print(user_input(user1))
