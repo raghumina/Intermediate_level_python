1103821387f2798c7067657ede07db6de4493cb1
raghu <raghumina11@gmail.com>
2020-09-27 20:14:02 +0530

    Create Join_func.py

---
+++ Join_func.py
@@ -0,0 +1,12 @@
+# Join function in python
+# generally join function is used in list set tuples when we need to join the strings or other data type
+# for example
+
+list1  = ["tom","cruz","hanary","cavill","johnny","deep"]
+for names in list1:
+    print(names)
+
+# now we need to join the names
+
+a = " and ".join(list1)
+print(a)
