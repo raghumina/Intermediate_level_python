739f6f8aa6e8d3a025f2d17306b175e9771ecdaa
raghu <raghumina11@gmail.com>
2020-10-06 17:42:16 +0530

    Create OOPs_1.py

---
+++ OOPs_1.py
@@ -0,0 +1,17 @@
+# Classes and Objects in python
+# Object Oriented Programming is a way of computer programming
+# using the idea of “objects” to represents data and methods.
+# A class is a collection of objects or you can say it is a blueprint of objects defining
+# the common attributes and behavior.
+# Now the question arises, how do you do that?
+
+# Now lets create a class
+
+class student :
+
+
+    def func(self):
+        print("hello")
+
+obj = student()
+print(obj.func())
