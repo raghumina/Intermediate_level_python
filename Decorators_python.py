248fe9dfda3dbda8ad19d46c5e80c8636915ba45
raghu <raghumina11@gmail.com>
2020-11-25 22:50:08 +0530

    Update Decorators_python.py

--- Decorators_python.py
+++ Decorators_python.py
@@ -156,3 +156,12 @@ newarr = arr[filter_arr]
 
 print(filter_arr)
 print(newarr)
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+
+plt.plot(xpoints, ypoints)
+plt.show()
