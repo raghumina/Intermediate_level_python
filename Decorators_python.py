20ed2bc798c16895c1a41812f4ba2bdbf3319866
raghu <raghumina11@gmail.com>
2020-11-30 22:25:17 +0530

    Update Decorators_python.py

--- Decorators_python.py
+++ Decorators_python.py
@@ -167,7 +167,13 @@ plt.show()
 import numpy as np
 
 arr = np.array([41, 42, 43, 44])
+import numpy as np
 
+arr = np.array([6, 7, 8, 9])
+
+x = np.searchsorted(arr, 7, side='right')
+
+print(x)
 # Create an empty list
 
 
