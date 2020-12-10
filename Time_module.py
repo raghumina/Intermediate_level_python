ceaf993f7c431e3e9bddc939b4676dbfe5c6db8a
raghu <raghumina11@gmail.com>
2020-12-10 22:25:06 +0530

    Update Time_module.py

--- Time_module.py
+++ Time_module.py
@@ -20,6 +20,49 @@ v = np.random.normal(size=100)
 print(skew(v))
 print(kurtosis(v))
 #
+
+
+# A Python program to demonstrate
+# inheritance
+
+
+# Base class or Parent class
+class Child:
+
+    # Constructor
+    def __init__(self, name):
+        self.name = name
+
+        # To get name
+
+    def getName(self):
+        return self.name
+
+        # To check if this person is student
+
+    def isStudent(self):
+        return False
+
+
+# Derived class or Child class
+class Student(Child):
+
+    # True is returned
+    def isStudent(self):
+        return True
+
+
+# Driver code
+# An Object of Child
+std = Child("Ram")
+print(std.getName(), std.isStudent())
+
+# An Object of Student
+std = Student("Shivam")
+print(std.getName(), std.isStudent())
+
+
+
 n = 0
 initial_time = time.time()
 while n < 10:
