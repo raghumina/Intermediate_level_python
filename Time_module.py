554b807301b2e8b8edf2d299f6b9ec0b4255ac3a
raghu <raghumina11@gmail.com>
2020-12-11 21:53:02 +0530

    Update Time_module.py

--- Time_module.py
+++ Time_module.py
@@ -52,6 +52,35 @@ class Student(Child):
         return True
 
 
+# Python program to
+# demonstrate protected members
+
+
+# Creating a base class
+class Base:
+    def __init__(self):
+        # Protected member
+        self._a = 2
+
+
+# Creating a derived class
+class Derived(Base):
+    def __init__(self):
+        # Calling constructor of
+        # Base class
+        Base.__init__(self)
+        print("Calling protected member of base class: ")
+        print(self._a)
+
+
+obj1 = Derived()
+
+obj2 = Base()
+
+# Calling protected member
+# Outside class will  result in
+# AttributeError
+print(obj2.a)
 # Driver code
 # An Object of Child
 std = Child("Ram")
