4207198154f447ae55d0b8f62395f20ea2fc97f2
raghu <raghumina11@gmail.com>
2020-12-27 22:49:30 +0530

    Update Time_module.py

--- Time_module.py
+++ Time_module.py
@@ -101,6 +101,25 @@ print(a)
 obj1 = Base()
 print(obj1.a)
 
+edit
+play_arrow
+
+brightness_4
+# Program to handle multiple errors with one except statement
+try:
+    a = 3
+    if a < 4:
+        # throws ZeroDivisionError for a = 3
+        b = a / (a - 3)
+
+        # throws NameError if a >= 4
+    print
+    "Value of b = ", b
+
+# note that braces () are necessary here for multiple exceptions
+except(ZeroDivisionError, NameError):
+    print
+    "\nError Occurred and Handled"
 
 # Uncommenting print(obj1.c) will
 # raise an AttributeError
