d7b374601f33c3b857bcca7dc14b06280d92b4f3
raghu <raghumina11@gmail.com>
2020-12-29 23:07:40 +0530

    Update Time_module.py

--- Time_module.py
+++ Time_module.py
@@ -122,6 +122,25 @@ except(ZeroDivisionError, NameError):
     "\nError Occurred and Handled"
 
 
+# Python program to illustrate functions
+# can be passed as arguments to other functions
+def shout(text):
+    return text.upper()
+
+
+def whisper(text):
+    return text.lower()
+
+
+def greet(func):
+    # storing the function in a variable
+    greeting = func("""Hi, I am created by a function 
+                    passed as an argument.""")
+    print(greeting)
+
+
+greet(shout)
+greet(whisper)
 # Uncommenting print(obj1.c) will
 # raise an AttributeError
 
