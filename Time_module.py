4bcb8183aa7b22fc96170a67b5be4c0edbb42b29
raghu <raghumina11@gmail.com>
2021-01-03 22:55:02 +0530

    Update Time_module.py

--- Time_module.py
+++ Time_module.py
@@ -237,6 +237,31 @@ class Student(Child):
         return True
 
 
+import re
+
+# \w is equivalent to [a-zA-Z0-9_].
+p = re.compile('\w')
+print(p.findall("He said * in some_lang."))
+
+# \w+ matches to group of alphanumeric character.
+p = re.compile('\w+')
+print(p.findall("I went to him at 11 A.M., he said *** in some_language."))
+
+# \W matches to non alphanumeric characters.
+p = re.compile('\W')
+print(p.findall("he said *** in some_language."))import re
+
+# \w is equivalent to [a-zA-Z0-9_].
+p = re.compile('\w')
+print(p.findall("He said * in some_lang."))
+
+# \w+ matches to group of alphanumeric character.
+p = re.compile('\w+')
+print(p.findall("I went to him at 11 A.M., he said *** in some_language."))
+
+# \W matches to non alphanumeric characters.
+p = re.compile('\W')
+print(p.findall("he said *** in some_language."))
 # Python program to
 # demonstrate protected members
 
