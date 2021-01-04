8187ab6fbc7802dd0f56157c4a32c22094edc87a
raghu <raghumina11@gmail.com>
2021-01-04 23:11:15 +0530

    Update Time_module.py

--- Time_module.py
+++ Time_module.py
@@ -249,7 +249,8 @@ print(p.findall("I went to him at 11 A.M., he said *** in some_language."))
 
 # \W matches to non alphanumeric characters.
 p = re.compile('\W')
-print(p.findall("he said *** in some_language."))import re
+print(p.findall("he said *** in some_language."))
+import re
 
 # \w is equivalent to [a-zA-Z0-9_].
 p = re.compile('\w')
@@ -262,6 +263,8 @@ print(p.findall("I went to him at 11 A.M., he said *** in some_language."))
 # \W matches to non alphanumeric characters.
 p = re.compile('\W')
 print(p.findall("he said *** in some_language."))
+
+
 # Python program to
 # demonstrate protected members
 
