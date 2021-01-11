b0e78ecdf402af9c2969eceda658b29e1862ac0d
raghu <raghumina11@gmail.com>
2021-01-11 23:07:11 +0530

    Update Time_module.py

--- Time_module.py
+++ Time_module.py
@@ -499,6 +499,18 @@ class Derived(Base):
 obj1 = Base()
 print(obj1.a)
 
+# A Python program to show different ways to create
+# Counter
+from collections import Counter
+
+# With sequence of items
+print(Counter(['B', 'B', 'A', 'B', 'C', 'A', 'B', 'B', 'A', 'C']))
+
+# with dictionary
+print(Counter({'A': 3, 'B': 5, 'C': 2}))
+
+# with keyword arguments
+print(Counter(A=3, B=5, C=2))
 # Uncommenting print(obj1.c) will
 # raise an AttributeError
 
